013e9182f20efd4ec6c4c48e35872f7819e21713
Divvie Esterhuizen <divvieesterhuizen@Divvies-MacBook-Air-2017-Big-Sur.local>
2021-07-29 17:49:45 +0200

    fix API const

--- src/App.js
+++ src/App.js
@@ -15,15 +15,7 @@ function App() {
   const [todos, setTodos] = useState([]);
   const [loading, setLoading] = useState(false);
 
-  try {
-    const API = process.env.REACT_APP_API;
-  } catch (err) {
-    if (!API) {
-      alert(
-        'API URL not found in process.env. Application will not work until you add an value for API in your environment variables.'
-      );
-    }
-  }
+  const API = process.env.REACT_APP_API;
 
   // initial run
   useEffect(() => {
